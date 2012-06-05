2e520f7b7a68bd5a9b3e59abeba26b79e1a490b1
Dan Di Spaltro <dan@cloudkick.com>
2012-06-04 23:23:12 -0700

    Add get_entity

---
+++ lib/rackspace-monitoring/monitoring/requests/get_entity.rb
@@ -0,0 +1,23 @@
+module Fog
+  module Monitoring
+    class Rackspace
+      class Real
+
+        def get_entity(entity_id)
+          request(
+            :expects  => [200, 203],
+            :method   => 'GET',
+            :path     => "entities/#{entity_id}"
+          )
+        end
+
+      end
+
+      class Mock
+
+      end
+    end
+  end
+end
+
+
