631102fc4d406551e90a03e78541be6de3494941
Dan Di Spaltro <dan@cloudkick.com>
2012-06-10 11:26:36 -0700

    Add an update path for entities

---
+++ lib/rackspace-monitoring/monitoring/requests/update_entity.rb
@@ -0,0 +1,22 @@
+module Fog
+  module Monitoring
+    class Rackspace
+      class Real
+
+        def update_entity(entity_id, options = {})
+          data = {}
+          data['label'] = options['label']
+          data['metadata'] = options['metadata']
+          data['ip_addresses'] = options['ip_addresses']
+          request(
+            :body     => MultiJson.encode(data),
+            :expects  => [201],
+            :method   => 'PUT',
+            :path     => "entities/#{entity_id}"
+          )
+        end
+      end
+    end
+  end
+end
+
