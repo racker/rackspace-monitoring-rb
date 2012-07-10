70c1fec9f9e5a6a8701452fb9ab7dad465e0b4fb
Tomaz Muraus <tomaz@tomaz.me>
2012-07-10 15:04:06 -0700

    Fix the syntax error.

--- lib/rackspace-monitoring.rb
+++ lib/rackspace-monitoring.rb
@@ -67,7 +67,7 @@ module Fog
           @rackspace_must_reauthenticate = false
           @connection_options = options[:connection_options] || {}
 
-          if options.has_key("raise_errors")
+          if options.has_key?("raise_errors")
               @raise_errors = options[:raise_errors]
           else
               @raise_errors = true
