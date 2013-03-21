991b9b9c9a85b3ca17055cc6bdebdc9300bd5e0c
Phil Kates <me@philkates.com>
2013-03-21 16:42:58 -0700

    Add variance, min, and max to DataPoint

--- lib/rackspace-monitoring/monitoring/models/data_point.rb
+++ lib/rackspace-monitoring/monitoring/models/data_point.rb
@@ -7,6 +7,9 @@ module Fog
       class DataPoint < Fog::Monitoring::Rackspace::Base
         attribute :num_points, :aliases => "numPoints"
         attribute :average
+        attribute :variance
+        attribute :min
+        attribute :max
         attribute :timestamp
         attribute :metric
       end
