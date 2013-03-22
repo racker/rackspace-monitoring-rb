64342acc1f4cd00fe9d735d473f587a0d2211b9d
Phil Kates <me@philkates.com>
2013-03-22 14:00:54 -0700

    Add check_types support
    Listing check_types is handy and will be useful for people.

---
+++ lib/rackspace-monitoring/monitoring/models/check_type.rb
@@ -0,0 +1,15 @@
+require 'rackspace-fog/core/model'
+require 'rackspace-monitoring/monitoring/models/base'
+
+module Fog
+  module Monitoring
+    class Rackspace
+      class CheckType < Fog::Monitoring::Rackspace::Base
+        identity :id
+        attribute :type
+        attribute :fields
+        attribute :channel
+      end
+    end
+  end
+end
---
+++ lib/rackspace-monitoring/monitoring/models/check_types.rb
@@ -0,0 +1,23 @@
+require 'rackspace-fog/core/collection'
+require 'rackspace-monitoring/monitoring/models/check_type'
+
+module Fog
+  module Monitoring
+    class Rackspace
+      class CheckTypes < Fog::Collection
+
+        model Fog::Monitoring::Rackspace::CheckType
+
+        def all
+          data = connection.list_check_types.body['values']
+          load(data)
+        end
+
+        def new(attributes = {})
+          super({ }.merge!(attributes))
+        end
+
+      end
+    end
+  end
+end
