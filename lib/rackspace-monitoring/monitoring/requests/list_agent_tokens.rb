4597fc8872f632dd68bab021f8ba0c017125ba84
adunn <alexanderldunn@gmail.com>
2013-03-27 10:16:53 -0700

    added functionality to follow next marker and return entire list of tokens

--- lib/rackspace-monitoring/monitoring/requests/list_agent_tokens.rb
+++ lib/rackspace-monitoring/monitoring/requests/list_agent_tokens.rb
@@ -3,11 +3,15 @@ module Fog
     class Rackspace
       class Real
 
-        def list_agent_tokens
+        def list_agent_tokens(opts={})
+          path = "agent_tokens"
+          opts.keys.each do |key|
+            path = path + "?#{key}=#{opts[key]}"
+          end
           request(
             :expects  => [200, 203],
             :method   => 'GET',
-            :path     => "agent_tokens"
+            :path     => path
           )
         end
 
